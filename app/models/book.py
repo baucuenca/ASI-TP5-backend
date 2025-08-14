efe7b15e78a7f6eb088a541e6fa87a3b88ea5beb
baucuenca <jnb.cuenca1507@gmail.com>
2025-08-14 19:40:32 -0300

    Comentarios

--- app/models/book.py
+++ app/models/book.py
@@ -1,9 +1,10 @@
-# Modelo Libro
+# Modelo de libro
 
 from sqlmodel import Field, SQLModel
 from typing import Optional 
 from datetime import datetime
 
+# Clase para el modelo de datos del libro en la BD
 class Book(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     title: str = Field(max_length=50)
@@ -11,6 +12,7 @@ class Book(SQLModel, table=True):
     published_year: int = Field(default=datetime.now().year)
     isbn: str = Field(max_length=13)
 
+# Clase para la creación de un nuevo libro
 class BookCreate(SQLModel):
     title: str = Field(max_length=50)
     author: str = Field(max_length=50)
--- app/routes/book.py
+++ app/routes/book.py
@@ -9,6 +9,7 @@ from ..config.db import session_dep
 # Rutas
 book = APIRouter()
 
+# Obtener todos los libros
 @book.get("/books")
 def read_books(session: session_dep):
     books = session.exec(select(Book)).all()
@@ -16,6 +17,7 @@ def read_books(session: session_dep):
         raise HTTPException(status_code=404, detail="No books found")
     return books
 
+# Obtener un libro por ID
 @book.get("/books/{book_id}")
 def read_book(book_id: int, session: session_dep):
     book = session.get(Book, book_id)
@@ -23,9 +25,10 @@ def read_book(book_id: int, session: session_dep):
         raise HTTPException(status_code=404, detail="Book not found")
     return book
 
+# Crear un nuevo libro
 @book.post("/books")
 def create_book(book: BookCreate, session: session_dep):
-    db_book = Book.model_validate(book)
+    db_book = Book.model_validate(book) # Validar y crear instancia de Book lista para insertar en la BD
     session.add(db_book)
     session.commit()
     session.refresh(db_book)
