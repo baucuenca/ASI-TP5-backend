9e45094b9f950ae2e156e1d00f7c169f7c322f1d
baucuenca <jnb.cuenca1507@gmail.com>
2025-08-24 01:04:24 -0300

    Solucionado problema de CORS para comunicarse con el frontend

--- app/main.py
+++ app/main.py
@@ -1,13 +1,32 @@
 # Archivo principal
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .routes.book import book
 from .routes.member import member
 from .routes.loan import loan
 from .config.db import create_tables, create_database
 
+from dotenv import load_dotenv
+import os
+
 app = FastAPI()
+
+load_dotenv()
+FRONTEND_ORIGIN = os.getenv("FRONTEND_ORIGIN")
+
+origins = [
+    FRONTEND_ORIGIN,
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(book)
 app.include_router(member)
